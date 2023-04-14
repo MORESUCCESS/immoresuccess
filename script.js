c6e3f3e2343aa8d0247423c980c10a9d065e150c
MORESUCCESS <130167357+MORESUCCESS@users.noreply.github.com>
2023-04-14 12:29:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//displaying the popup
+function div_show(){
+    document.getElementById('holdall').style.display="block";
+}
+//hiding the popup
+function div_hide(){
+    document.getElementById('holdall').style.display="none";
+}
